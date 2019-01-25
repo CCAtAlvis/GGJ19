eccbae411d4e83973bcb2feec775f3226878ccf3
Mithil467 <mithil467@gmail.com>
2019-01-25 23:34:37 +0530

    Character Movement added

---
+++ Home Sweet Home/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        //if(Input.GetKey(KeyCode.Space))
+        //{
+
+        //}
+    }
+}
