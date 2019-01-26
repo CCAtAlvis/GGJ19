fb2a2a1b9282ec1491a27b54cc3d3fc009e75d72
Mithil467 <mithil467@gmail.com>
2019-01-26 16:23:46 +0530

    Fading into memories done

--- Home Sweet Home/Assets/Scripts/CharacterMovement.cs
+++ Home Sweet Home/Assets/Scripts/CharacterMovement.cs
@@ -1,48 +1,48 @@
 ﻿using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
-    
+    public bool disableMovement = false;
     public float speed = 0.5f;
     public float speedRotation = 2f;
     
 	Animator ab;
-    private Rigidbody rb;
-
 
 
     void Start()
     {
 		ab = GetComponent<Animator> ();
-		rb = GetComponent<Rigidbody>();
     }
 
     void FixedUpdate()
     {
-		float moveVertical = Input.GetAxis ("Vertical");
-			if ( moveVertical< 0)
-			moveVertical = 0;
-		
-		Vector3 movement = new Vector3(0, 0.0f, moveVertical);
-        
-        transform.Translate(movement * speed * Time.deltaTime);
-
-        Plane playerPlane = new Plane(Vector3.up, transform.position);
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        float hitdist = 0.0f;
-
-        if (playerPlane.Raycast(ray, out hitdist))
+        if (!disableMovement)
         {
-            Vector3 targetPoint = ray.GetPoint(hitdist);
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            float moveVertical = Input.GetAxis("Vertical");
+            if (moveVertical < 0)
+                moveVertical = 0;
 
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotation * Time.deltaTime);
-        }
+            Vector3 movement = new Vector3(0, 0.0f, moveVertical);
 
-		if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
-        {
-			ab.SetInteger ("mov", 1);
+            transform.Translate(movement * speed * Time.deltaTime);
+
+            Plane playerPlane = new Plane(Vector3.up, transform.position);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float hitdist = 0.0f;
+
+            if (playerPlane.Raycast(ray, out hitdist))
+            {
+                Vector3 targetPoint = ray.GetPoint(hitdist);
+                Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speedRotation * Time.deltaTime);
+            }
+
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            {
+                ab.SetInteger("mov", 1);
+            }
+            else
+                ab.SetInteger("mov", 0);
         }
-		else
-			ab.SetInteger ("mov", 0);
     }
 }
--- Home Sweet Home/Assets/Scripts/Memories.cs
+++ Home Sweet Home/Assets/Scripts/Memories.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Memories : MonoBehaviour {
+    public GameObject obj,player;
+    public float FadeRate;
+    public Image image;
+    private float targetAlpha;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
+        Color curColor = image.color;
+        float alphaDiff = Mathf.Abs(curColor.a - targetAlpha);
+        if (alphaDiff > 0.0001f)
+        {
+            curColor.a = Mathf.Lerp(curColor.a, targetAlpha, FadeRate * Time.deltaTime);
+            image.color = curColor;
+        }
+    }
+
+    public void FadeOut()
+    {
+        targetAlpha = 0.0f;
+    }
+
+    public void FadeIn()
+    {
+        targetAlpha = 1.0f;
+    }
+
+    private void OnTriggerEnter(Collider col)
+	{
+        player.GetComponent<CharacterMovement>().disableMovement = true;
+        obj.SetActive(true);
+        targetAlpha = image.color.a;
+        FadeIn();
+    }
 }
