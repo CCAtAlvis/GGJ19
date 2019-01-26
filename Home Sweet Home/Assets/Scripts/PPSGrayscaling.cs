1efdb3874807ecfbcc14a83d6b72e652d9d0ed37
Chinmay Chandak <chandakchinmay@gmail.com>
2019-01-27 01:19:07 +0530

    added PostProcessing behaviour controller: WiP
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Home Sweet Home/Assets/Scripts/PPSGrayscaling.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class PPSGrayscaling : MonoBehaviour
+{
+    public PostProcessingProfile profile;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorGradingModel.Settings cgs = profile.colorGrading.settings;
+        cgs.basic.saturation = Mathf.Abs(Mathf.Sin(Time.time));
+
+        profile.colorGrading.settings = cgs;
+    }
+}
