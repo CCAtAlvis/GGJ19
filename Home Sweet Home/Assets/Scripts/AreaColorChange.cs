13e5324bbddba3312c544078f547c49a962da356
Mithil467 <mithil467@gmail.com>
2019-01-26 11:54:37 +0530

    Implemented mat change

--- Home Sweet Home/Assets/Scripts/AreaColorChange.cs
+++ Home Sweet Home/Assets/Scripts/AreaColorChange.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class AreaColorChange : MonoBehaviour {
 
-    private Material mat;
-    void Start()
-    {
-        mat = GetComponent<Renderer>().material;
+    public Material[] mat1;
+    public Material[] mat2;
+    public GameObject[] players;
+    bool ismat1 = true;
 
-    }
-	public void changeall()
+    private void OnTriggerEnter(Collider col)
     {
-
+        if(ismat1)
+            for (int i = 0; i < players.Length; i++)
+            {
+                mat1[i] = players[i].GetComponent<Renderer>().material;
+                players[i].GetComponent<Renderer>().material = mat2[i];
+                ismat1 = false;
+           // gameObject.SetActive(false);
+            }
+        else
+            for (int i = 0; i < players.Length; i++)
+            {
+                players[i].GetComponent<Renderer>().material = mat1[i];
+                ismat1 = true;
+                // gameObject.SetActive(false);
+            }
     }
+
 }
