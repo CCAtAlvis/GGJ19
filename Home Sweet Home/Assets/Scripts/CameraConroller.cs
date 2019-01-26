dcb32bfaadcd3eaf001a1e10088a266303ae39d8
Chinmay Chandak <chandakchinmay@gmail.com>
2019-01-26 20:06:13 +0530

    Added camera controller
    Signed-off-by: Chinmay Chandak, @CCAtAlvis

---
+++ Home Sweet Home/Assets/Scripts/CameraConroller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraConroller : MonoBehaviour
+{
+    public Vector3 cameraPositionOffset;
+    public Transform playerTransform;
+
+    void Update()
+    {
+        if (playerTransform != null)
+        {
+            transform.position = playerTransform.position + cameraPositionOffset;
+        }
+    }
+}
