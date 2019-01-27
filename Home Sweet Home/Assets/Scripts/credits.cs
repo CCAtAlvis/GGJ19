4f98e932528d46b04a90cf6d7d6fc39083a0deb4
barathnadar <nadar.barath18@siesgst.ac.in>
2019-01-27 14:37:19 +0530

    credits

--- Home Sweet Home/Assets/Scripts/credits.cs
+++ Home Sweet Home/Assets/Scripts/credits.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class credits : MonoBehaviour {
-
+	public float delay =0;
+	public string NewLevel="MenuLevel";
 	// Use this for initialization
 	void Start () {
-		
+		Invoke ("caller", 10f);
 	}
-	
+	public void caller(){
+		SceneManager.LoadScene (NewLevel);
+
+	}
+
 	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
